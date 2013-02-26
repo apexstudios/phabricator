633ac0f83594df51be58e5e4db630f986e2f1fb1
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-02-26 10:53:55 +0100

    adding Comments controller for packager
    Summary: Refs T178 - restoring transactions, tansaction type and transaction editor, too
    
    Test Plan: well, the comment transactions appear - without comments
    
    Reviewers: anh-chrome
    
    Reviewed By: anh-chrome
    
    Maniphest Tasks: T178
    
    Differential Revision: http://phabricator.burningreality.de/D122

--- src/applications/packager/editor/PhabricatorPackagerEditor.php
+++ src/applications/packager/editor/PhabricatorPackagerEditor.php
@@ -3,6 +3,65 @@
 final class PhabricatorPackagerEditor
   extends PhabricatorApplicationTransactionEditor {
 
+  public function getTransactionTypes() {
+    $types = parent::getTransactionTypes();
+
+    $types[] = PhabricatorTransactions::TYPE_COMMENT;
+    $types[] = PhabricatorPackagerTransactionType::TYPE_DOWNLOAD;
+
+    return $types;
+  }
+
+  protected function getCustomTransactionOldValue(
+    PhabricatorLiskDAO $object,
+    PhabricatorApplicationTransaction $xaction) {
+
+    switch ($xaction->getTransactionType()) {
+      case PhabricatorPackagerTransactionType::TYPE_DOWNLOAD:
+        return $object->getDownloads();
+    }
+  }
+
+  protected function getCustomTransactionNewValue(
+    PhabricatorLiskDAO $object,
+    PhabricatorApplicationTransaction $xaction) {
+
+    switch ($xaction->getTransactionType()) {
+      case PhabricatorPackagerTransactionType::TYPE_DOWNLOAD:
+        return $xaction->getNewValue() + 1;
+    }
+  }
+
+  protected function applyCustomInternalTransaction(
+    PhabricatorLiskDAO $object,
+    PhabricatorApplicationTransaction $xaction) {
+
+    switch ($xaction->getTransactionType()) {
+      case PhabricatorPackagerTransactionType::TYPE_DOWNLOAD:
+        $object->setDownloads($xaction->getNewValue());
+        break;
+    }
+  }
+
+  protected function applyCustomExternalTransaction(
+    PhabricatorLiskDAO $object,
+    PhabricatorApplicationTransaction $xaction) {
+    return;
+  }
+
+  protected function mergeTransactions(
+    PhabricatorApplicationTransaction $u,
+    PhabricatorApplicationTransaction $v) {
+
+    $type = $u->getTransactionType();
+    switch ($type) {
+      case PhabricatorPackagerTransactionType::TYPE_DOWNLOAD:
+        return $v;
+    }
+
+    return parent::mergeTransactions($u, $v);
+  }
+
   protected function supportsMail() {
     return false;
   }
--- src/applications/packager/storage/PhabricatorPackagerTransaction.php
+++ src/applications/packager/storage/PhabricatorPackagerTransaction.php
@@ -19,6 +19,67 @@ final class PhabricatorPackagerTransaction
     return pht('packager');
   }
 
+  public function getTitle() {
+    $author_phid = $this->getAuthorPHID();
+
+    switch ($this->getTransactionType()) {
+      case PhabricatorPackagerTransactionType::TYPE_DOWNLOAD:
+        return pht(
+          '%s downloaded this package.',
+          $this->renderHandleLink($author_phid));
+        break;
+    }
+
+    return parent::getTitle();
+  }
+
+  public function getTitleForFeed() {
+    if ($this->getTransactionType() ==
+      PhabricatorPackagerTransactionType::TYPE_DOWNLOAD) {
+      return $this->getTitle();
+    } else {
+      return parent::getTitleForFeed();
+    }
+  }
+
+  public function getActionName() {
+    $old = $this->getOldValue();
+    $new = $this->getNewValue();
+
+    switch ($this->getTransactionType()) {
+      case PhabricatorPackagerTransactionType::TYPE_DOWNLOAD:
+        return pht('Downloaded');
+        break;
+    }
+
+    return parent::getActionName();
+  }
+
+  public function getActionStrength() {
+    switch ($this->getTransactionType()) {
+      case PhabricatorPackagerTransactionType::TYPE_DOWNLOAD:
+        return 1.5;
+    }
+    return parent::getActionStrength();
+  }
+
+  public function getIcon() {
+    if ($this->getTransactionType() ==
+      PhabricatorPackagerTransactionType::TYPE_DOWNLOAD) {
+      return 'view';
+    } else {
+      return parent::getIcon();
+    }
+  }
+
+  public function getColor() {
+    if ($this->getTransactionType() ==
+      PhabricatorPackagerTransactionType::TYPE_DOWNLOAD) {
+      return PhabricatorTransactions::COLOR_SKY;
+    } else {
+      return parent::getColor();
+    }
+  }
 
 }
 
