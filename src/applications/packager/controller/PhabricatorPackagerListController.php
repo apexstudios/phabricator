95df7ad26cad86afc3149db0c24fd2bd89719170
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-02-26 03:22:37 +0100

    adding the list view for packager/

---
+++ src/applications/packager/controller/PhabricatorPackagerListController.php
@@ -0,0 +1,76 @@
+<?php
+
+final class PhabricatorPackagerListController
+  extends PhabricatorPackagerController {
+
+  public function processRequest() {
+
+    $request = $this->getRequest();
+
+    $packageTable = new PhabricatorFilePackage();
+
+    $pager = new AphrontPagerView();
+    $pager->setOffset($request->getInt('page'));
+    $conn = $packageTable->establishConnection('r');
+
+    $pager = new AphrontPagerView();
+    $pager->setOffset($request->getInt('page'));
+
+    $packageEntries = $packageTable->loadAllWhere(
+      '1 = 1 ORDER BY id DESC LIMIT %d, %d',
+      $pager->getOffset(),
+      $pager->getPageSize());
+
+    // Get an exact count since the size here is reasonably going to be a few
+    // thousand at most in any reasonable case.
+    $count = queryfx_one(
+      $conn,
+      'SELECT COUNT(*) N FROM %T',
+      $packageTable->getTableName());
+    $count = $count['N'];
+
+    $pager->setCount($count);
+    $pager->setURI($request->getRequestURI(), 'page');
+
+    $nodata = pht('There are no image macros yet.');
+
+    $list = new PhabricatorObjectItemListView();
+    $list->setStackable();
+
+    foreach ($packageEntries as $package) {
+      $item = id(new PhabricatorObjectItemView())
+        ->setHeader($package->getPackageUrl());
+
+      $list->addItem($item);
+    }
+
+    $header = id(new PhabricatorHeaderView())
+      ->setHeader("Registered Packages");
+
+    $nav = $this->buildSideNavView($for_app = false);
+
+    $nav->appendChild(array(
+        $header,
+        $list,
+        $pager,
+    ));
+
+    $name = pht('All Packages');
+
+    $crumbs = $this->buildApplicationCrumbs();
+    $crumbs->addCrumb(
+      id(new PhabricatorCrumbView())
+        ->setName($name)
+        ->setHref($request->getRequestURI()));
+    $nav->setCrumbs($crumbs);
+
+    $nav->selectFilter('/');
+
+    return $this->buildApplicationPage(
+      $nav,
+      array(
+        'device' => true,
+        'title' => pht('File Packages'),
+      ));
+  }
+}
