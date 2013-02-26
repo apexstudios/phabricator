31479ffecdf380232e69565a70302855782be03a
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-02-26 14:38:20 +0100

    some error handling for conduit packager.register

--- src/applications/packager/conduit/ConduitAPI_packager_register_Method.php
+++ src/applications/packager/conduit/ConduitAPI_packager_register_Method.php
@@ -22,12 +22,17 @@ final class ConduitAPI_packager_register_Method extends
 
   public function defineErrorTypes() {
     return array(
+      'ERR-EMPTY-URI' => 'Empty URI',
     );
   }
 
   protected function execute(ConduitAPIRequest $request) {
     $user = $request->getUser();
     $url = $request->getValue('url');
+
+    if (empty($url)) {
+      throw new ConduitException("ERR-EMPTY-URI");
+    }
 
     $package = new PhabricatorFilePackage();
     $package->setAuthorPHID($user->getPHID());
