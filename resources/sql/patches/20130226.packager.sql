c31a0f48eead17701ce57125b006a64bbbc48a38
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-02-26 05:10:42 +0100

    adding download count

--- resources/sql/patches/20130226.packager.sql
+++ resources/sql/patches/20130226.packager.sql
@@ -3,6 +3,7 @@ CREATE TABLE {$NAMESPACE}_packager.packager_filepackage (
   phid VARCHAR(64) NOT NULL COLLATE utf8_bin,
   authorPHID VARCHAR(64) NOT NULL COLLATE utf8_bin,
   packageUrl VARCHAR(255) NOT NULL COLLATE utf8_general_ci,
+  downloads INT UNSIGNED NOT NULL DEFAULT 0,
   dateCreated INT UNSIGNED NOT NULL,
   dateModified INT UNSIGNED NOT NULL,
   KEY `key_author` (authorPHID),
