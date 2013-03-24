e8b72d5cbb7f50257d6998bf2ed9a2ef2dfb046e
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-03-24 23:02:41 +0100

    Fixed bad merge

--- src/applications/phid/PhabricatorPHIDConstants.php
+++ src/applications/phid/PhabricatorPHIDConstants.php
@@ -36,8 +36,6 @@ final class PhabricatorPHIDConstants {
 
   const PHID_TYPE_PCKG    = 'PCKG';
 
-  const PHID_TYPE_PCKG    = 'PCKG';
-
   const PHID_TYPE_XACT    = 'XACT';
   const PHID_TYPE_XCMT    = 'XCMT';
 
